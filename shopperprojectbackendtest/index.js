ddb39018f30a5de91894a8c5d74c1310576196ad
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-01-05 11:46:34 +0530

    added router file to test and nodemon to restarts server automatically after saves my file

--- shopperprojectbackendtest/index.js
+++ shopperprojectbackendtest/index.js
@@ -2,6 +2,14 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+//test to route a file using postman
+
+app.get("/home",(req,res)=>{
+    res.json({
+        message:"user can access home field"
+    })
+})
+
 app.listen(port,()=>{
     console.log(`The port is running on port ${port}`)
 })
