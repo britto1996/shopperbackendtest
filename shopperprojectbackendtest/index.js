803bd3f38c90b41cf10b2b3c3edf09e2bcc10407
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-01-05 12:15:27 +0530

    more routes added for test

--- shopperprojectbackendtest/index.js
+++ shopperprojectbackendtest/index.js
@@ -10,6 +10,24 @@ app.get("/home",(req,res)=>{
     })
 })
 
+app.get("/signup",(req,res)=>{
+    res.json({
+        message:"user registered successfully"
+    })
+})
+
+app.get("/signin",(req,res)=>{
+    res.json({
+        message:"user can access dashboard"
+    })
+})
+
+app.get("/signout",(req,res)=>{
+    res.json({
+        message:"user loggedout successfully"
+    })
+})
+
 app.listen(port,()=>{
     console.log(`The port is running on port ${port}`)
 })
