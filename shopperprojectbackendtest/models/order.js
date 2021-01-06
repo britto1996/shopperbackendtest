52c3c5e9fe75a87137c3aea9d1a2d9b80dd79df9
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-01-06 13:06:38 +0530

    full model of ecommerce will be finished come back for some editing purposes

--- shopperprojectbackendtest/models/order.js
+++ shopperprojectbackendtest/models/order.js
@@ -1,7 +1,43 @@
 const mongoose = require("mongoose")
+const {ObjectId} = mongoose.Schema
 
+const ProductCartSchema = new mongoose.Schema({
+    product:{
+        type:{ObjectId},
+        ref:"Product"
+    },
+    name:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    count:{
+        type:Number
+    },
+    amount:{
+        type:Number
+    }
+})
 const orderSchema = new mongoose.Schema({
-
+    products:[ProductCartSchema],
+    transcaction_id:{
+        type:Number
+    },
+    order:{
+        type:Number
+    },
+    product:{
+        type:{ObjectId},
+        ref:"Product"
+    },
+    user:{
+        type:{ObjectId},
+        ref:"User"
+    },
+    address:{
+        type:String
+    }
 })
 
 module.exports = mongoose.model("Order",orderSchema)
