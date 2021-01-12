0394aaa3f23f8beb7f99e026ecaec682768275f0
LAPTOP-FV0JU91J\BTM WORK TIME <btm15037@gmail.com>
2021-01-12 23:40:16 +0530

    added category updated work to the backend

--- shopperprojectbackendtest/controllers/category.js
+++ shopperprojectbackendtest/controllers/category.js
@@ -1,4 +1,4 @@
-const category = require("../models/category")
+
 const Category = require("../models/category")
 
 exports.getCategoryById = (req,res,next,id)=>{
@@ -26,5 +26,34 @@ exports.createCategory = (req,res)=>{
         res.json({
             message:category
         })
+    })
+}
+
+exports.getCategory = (req,res)=>{
+    return res.json(req.category)
+}
+
+exports.getAllCategories = (req,res)=>{
+    Category.find().exec((err,categories)=>{
+        if(err){
+           return res.status(400).json({
+                err:"Categories not found"
+            })
+        }
+        res.json(categories)
+    })
+}
+
+exports.updatedCategory = (req,res)=>{
+    const category = req.category
+    const updatedCate = req.body.category.name
+
+    category.save((err,updateItems)=>{
+        if(err){
+            res.status(400).json({
+                err:"category doesn't updated"
+            })
+        }
+        res.json(updateItems)
     })
 }
--- shopperprojectbackendtest/routes/category.js
+++ shopperprojectbackendtest/routes/category.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryById, createCategory } = require("../controllers/category");
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategories,
+  updatedCategory,
+} = require("../controllers/category");
 const {
   isSignedIn,
   isAuthentication,
@@ -12,6 +18,8 @@ const { getUserById } = require("../controllers/user");
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
 
+//create
+
 router.post(
   "/category/create/:userId",
   isSignedIn,
@@ -19,5 +27,21 @@ router.post(
   isAdmin,
   createCategory
 );
+
+//read
+
+router.get("/category/:categoryId", getCategory);
+router.get("/categories", getAllCategories);
+
+//update
+router.put(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthentication,
+  isAdmin,
+  updatedCategory
+);
+
+//delete
 
 module.exports = router;
